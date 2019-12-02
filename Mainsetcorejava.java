0c11e2679c5d161972ea7f72c5cfa9a44b96e783
9061711428 <54677210+9061711428@users.noreply.github.com>
2019-12-02 14:33:17 +0530

    Add files via upload

---
+++ Mainsetcorejava.java
@@ -0,0 +1,40 @@
+package myprogramz;
+import java.util.Scanner;
+public class Mainsetcorejava {
+	
+	public static void main(String args[]) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter your name ");
+	String name=sc.nextLine();
+	System.out.println("Enter your address ");
+	String address=sc.nextLine();
+	System.out.println("Enter your contact number ");
+	String contactNumber=sc.nextLine();
+	System.out.println("Enter your email ");
+	
+	String email=sc.nextLine();
+	
+	System.out.println("Enter your proof type ");
+	String proofTyp=sc.nextLine();
+	
+	System.out.println("Enter your proofid ");
+	String proofID=sc.nextLine();
+	corejavagetname obj=new corejavagetname();
+	obj.setname(name);
+	obj.setaddress(address);
+	obj.setcontactNumber(contactNumber);
+	obj.setemail(email);
+	obj.setproofTyp(proofTyp);
+	obj.setproofID(proofID);
+	
+	System.out.println("name is\t:"+obj.getname());
+	System.out.println("address is\t:"+obj.getaddress());
+	System.out.println("contact is\t:"+obj.getcontactNumber());
+	System.out.println("Email is\t:"+obj.getemail());
+	System.out.println("proof type is\t:"+obj.getproofTyp());
+	System.out.println("proof id is\t:"+obj.getproofID());
+	System.out.println("Thankyou for registration.Your ID is 1");
+	
+	}
+
+}
---
+++ corejavagetname.java
@@ -0,0 +1,74 @@
+package myprogramz;
+import java.util.Scanner;
+public class corejavagetname {
+	String name;
+	String address;
+	String contactNumber;
+	String email;
+	String proofTyp;
+	String proofID;
+	public void setname(String name)
+	{
+		this.name=name;
+		
+	}
+	public void setaddress(String address)
+	{
+		this.address=address;
+		
+	}
+	public void setcontactNumber(String contactNumber)
+	{
+		this.contactNumber=contactNumber;
+		
+	}
+	public void setemail(String email)
+	{
+		this.email=email;
+		
+	}
+	public void setproofTyp(String proofTyp)
+	{
+		this.proofTyp=proofTyp;
+		
+	}
+	public void setproofID(String proofID)
+	{
+		this.proofID=proofID;
+		
+	}
+	public String getname()
+	{
+		return this.name;
+		
+	}
+	public String getaddress()
+	{
+		return this.address;
+		
+	}
+	public String getcontactNumber()
+	{
+		return this.contactNumber;
+		
+	}
+	public String getemail()
+	{
+		return this.email;
+		
+	}
+	public String getproofTyp()
+	{
+		return this.proofTyp;
+		
+	}
+	public String getproofID()
+	{
+		return this.proofID;
+		
+	}
+	
+	
+	
+
+}
