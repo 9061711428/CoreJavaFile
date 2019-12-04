0b87feaba8f1e53e82046b1550a962bee55deb74
9061711428 <54677210+9061711428@users.noreply.github.com>
2019-12-04 10:32:59 +0530

    Add files via upload

---
+++ ExampleINterface.java
@@ -0,0 +1,34 @@
+package myprogramz;
+interface Example
+{	
+	void method1();
+	void method2();
+	void method3();
+}
+
+public class ExampleINterface implements Example{ 
+	public void method1()
+	{
+	System.out.println("It is Method 1");
+	}
+	public void method2()
+	{
+	System.out.println("It is Method 2");
+	}
+	public void method3()
+	{
+	System.out.println("It is Method 3");
+	}
+	public static void main(String args[])
+	
+	{
+		ExampleINterface obj=new ExampleINterface();
+		obj.method1();
+		obj.method2();
+		obj.method3();
+		
+	}
+	
+	
+
+}
