307b1468554a4186dc5ccf7c0d85b41297c95838
s@1996 <54677210+9061711428@users.noreply.github.com>
2020-01-04 13:00:03 +0530

    Update AbstractExample1.java

--- AbstractExample1.java
+++ AbstractExample1.java
@@ -1,46 +1,41 @@
 package myprogramz;
 
-public class AbstractExample1{
+public class AbstractExample1
+{
 	
-		public static void main(String args[])
-		{
-		Racoon racoon=new Racoon("Racoon");
-		racoon.eat();
-		racoon.breathe();
-		}
-		}
-		abstract class Food{//abstract class
-
-		private String name;
-		public Food  (String name) {
-		this.name=name;
-		}
-		public abstract void eat();//abstract method
-		public abstract void breathe();//abstract method
-		public String getName(){
-		return name;
-		}
-		}
-		class Racoon extends Food{
-		public Racoon(String name){
-		super(name);//call superclass 
-		}
-		public void eat(){
-		System.out.println(getName()+" is eating");
-		}
-
-
-
-		public void breathe(){
-		System.out.println("breathe in,breathe out,repeat");
-		}
-
-
-
-
-		
-		
-		}
+	public static void main(String args[])
+	{
+	Racoon racoon=new Racoon("Racoon");
+	racoon.eat();		
+	racoon.breathe();
+	}
+	abstract class Food{//abstract class
+	private String name;
+	public Food  (String name) 
+	{
+	this.name=name;
+	}
+	public abstract void eat();//abstract method
+	public abstract void breathe();//abstract method
+	public String getName()
+	{
+	return name;
+	}
+}
+	class Racoon extends Food
+	{
+	public Racoon(String name)
+	{
+	super(name);//call superclass 
+	}
+	public void eat()
+	{
+	System.out.println(getName()+" is eating");
+	}
+	public void breathe(){
+	System.out.println("breathe in,breathe out,repeat");
+	}
+	}
 
 
 
